0f8aac1dc26aa52092c310750a625a0359f50500
Binyam Teklu <binyamteklu@Binyams-Mac-mini.local>
2014-12-19 12:43:54 -0800

    initial commit comment

--- SequenceAnimationTest/XYZAppDelegate.m
+++ SequenceAnimationTest/XYZAppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "XYZAppDelegate.h"
+#import "XYZViewController.h"
 
 @implementation XYZAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[XYZViewController alloc] init];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
--- SequenceAnimationTest/XYZViewController.m
+++ SequenceAnimationTest/XYZViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "XYZViewController.h"
+#import <pop/POP.h>
+#define MCANIMATE_SHORTHAND
+#import "POP+MCAnimate.h"
 
 @interface XYZViewController ()
+
+@property (strong, nonatomic) UITableView *tableview;
 
 @end
 
@@ -18,7 +23,62 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    
+    _tableview = [[UITableView alloc] initWithFrame:CGRectMake(20, 30, 200, 300)];
+    _tableview.delegate = self;
+    _tableview.dataSource = self;
+    [self.view addSubview:_tableview];
+    
+    [_tableview registerClass:[UITableViewCell class] forCellReuseIdentifier:@"reuseId"];
 }
+
+
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseId" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [@"row " stringByAppendingFormat:@"%d", indexPath.row];
+    cell.textLabel.text = [cell.textLabel.text stringByAppendingFormat:@"%@ %@", cell.textLabel.text, cell.textLabel.text];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSArray *visibleCells = [_tableview visibleCells];
+    
+    [visibleCells sequenceWithInterval:0.5 animations:^(UIView *cell, NSInteger index){
+        CALayer *cellLayer = cell.layer;
+        
+        cellLayer.springSpeed = 2;
+        
+        cellLayer.spring.positionX = cellLayer.position.x - cellLayer.bounds.size.width;
+        
+        
+    } completion:^(BOOL finished){
+        [_tableview reloadData];
+    }];
+}
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
